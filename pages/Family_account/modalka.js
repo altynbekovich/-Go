831bd900fc802ac8f7008f527195a6c2db43f30d
Aidar-hub <146725346+Aidar-hub@users.noreply.github.com>
2023-11-17 22:44:45 +0600

    ds

--- pages/Family_account/modalka.js
+++ pages/Family_account/modalka.js
@@ -1,21 +1,3 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("modalBtn");
-var span = document.getElementById("closeBtn");
-
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-span.onclick = function () {
-  modal.style.display = "none";
-  console.log("ghvjbk");
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
 
 var modalTresh = document.getElementById("modalor");
 var btnTresh = document.getElementById("modalor-show");
